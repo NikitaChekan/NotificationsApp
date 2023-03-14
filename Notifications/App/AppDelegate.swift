67a3afcdb4673386d9d6cb95bcba6acc59697b6a
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2023-03-14 18:52:35 +0300

    notification handling

--- Notifications/App/AppDelegate.swift
+++ Notifications/App/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         requestAuthorization()
+        notificationCenter.delegate = self
         return true
     }
 
@@ -60,6 +61,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             }
         }
     }
+}
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+     
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        if response.notification.request.identifier == "Local Notification" {
+            print("Handling notification with the Local Notification Identifire")
+        }
+        
+        completionHandler()
+    }
+    
 }
 
